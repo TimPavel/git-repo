d6e5135f7dd160f69293d28ac98382a427ee51a7
TimPavel <timpn@yandex.ru>
2018-08-31 20:42:35 +0300

    My HomeWork after first lesson JS-2

---
+++ script.js
@@ -0,0 +1,77 @@
+function Container(id, className) {
+  this.id = id;
+  this.class = className;
+}
+
+Container.prototype.render = function() {
+  var div = document.createElement('div');
+
+  div.className = this.className;
+  div.id = this.id;
+
+  return div;
+};
+
+
+function Menu(id, className, items) {
+  Container.call(this, id, className);
+  this.items = items;
+}
+
+Menu.prototype = Object.create(Container.prototype);
+Menu.prototype.render = function() {
+  var ul = document.createElement('ul');
+  ul.className = this.class;
+  ul.id = this.id;
+
+  this.items.forEach(function(item) {
+    if (item instanceof Container) {
+      ul.appendChild(item.render());
+    }
+  });
+
+  return ul;
+};
+
+Menu.prototype.remove = function() {
+	var elem = document.querySelector('ul');
+	elem.remove();
+};
+
+function SubMenu() {
+// нет никаких мыслей(
+}
+
+SubMenu.prototype = Object.create(Menu.prototype);
+SubMenu.prototype.render = function() {
+
+};
+
+
+function MenuItem(href, label) {
+  Container.call(this, '', 'menu-item');
+
+  this.href = href;
+  this.label = label;
+}
+
+MenuItem.prototype = Object.create(Container.prototype);
+MenuItem.prototype.render = function() {
+	var li = document.createElement('li');
+	var a = document.createElement('a');
+
+	a.href = this.href;
+	a.textContent = this.label;
+
+	li.appendChild(a);
+	li.className = this.class;
+
+	return li;
+};
+
+MenuItem.prototype.remove = function() {
+	var elem = document.querySelector('.menu-item');
+	elem.remove();
+};
+
+
